35619ab81ab905959c6179b87c7567fdaf5eb953
Jason Romero <codenamejason@outlook.com>
2021-09-17 18:15:55 -0400

    update contracts and add comments

--- packages/hardhat/contracts/SimpleStream.sol
+++ packages/hardhat/contracts/SimpleStream.sol
@@ -1,9 +1,11 @@
 pragma solidity >=0.8.0;
 //SPDX-License-Identifier: MIT
 
-// import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title Simple Stream Contract
+/// @author ghostffcode
+/// @notice the meat and potatoes of the stream
 contract SimpleStream {
     event Withdraw(address indexed to, uint256 amount, string reason);
     event Deposit(address indexed from, uint256 amount, string reason);
@@ -11,8 +13,8 @@ contract SimpleStream {
     address payable public toAddress; // = payable(0xD75b0609ed51307E13bae0F9394b5f63A7f8b6A1);
     uint256 public cap; // = 0.5 ether;
     uint256 public frequency; // 1296000 seconds == 2 weeks;
-    uint256 public last; //stream starts empty (last = block.timestamp) or full (block.timestamp - frequency)
-    IERC20 public gtc;
+    uint256 public last; // stream starts empty (last = block.timestamp) or full (block.timestamp - frequency)
+    IERC20 public gtc; 
 
     constructor(
         address payable _toAddress,
@@ -32,6 +34,8 @@ contract SimpleStream {
         }
     }
 
+    /// @dev get the balance of a stream
+    /// @return the balance of the stream
     function streamBalance() public view returns (uint256) {
         if (block.timestamp - last > frequency) {
             return cap;
@@ -39,6 +43,9 @@ contract SimpleStream {
         return (cap * (block.timestamp - last)) / frequency;
     }
 
+    /// @dev withdraw from a stream
+    /// @param amount amount of withdraw
+    /// @param reason reason for withdraw
     function streamWithdraw(uint256 amount, string memory reason) external {
         require(msg.sender == toAddress, "this stream is not for you");
         uint256 totalAmountCanWithdraw = streamBalance();
@@ -54,6 +61,10 @@ contract SimpleStream {
         require(gtc.transfer(msg.sender, amount), "Transfer failed");
     }
 
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param reason reason for deposit
+    /// @param  value the amount of the deposit
     function streamDeposit(string memory reason, uint value) external {
         require(value >= cap / 10, "Not big enough, sorry.");
         require(
