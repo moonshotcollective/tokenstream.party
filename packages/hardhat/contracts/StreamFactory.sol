00fa1a06e784a5afe2de45b2908cad40b45fa2bc
Jason Romero <codenamejason@outlook.com>
2021-11-05 17:30:24 -0400

    one more update

--- packages/hardhat/contracts/StreamFactory.sol
+++ packages/hardhat/contracts/StreamFactory.sol
@@ -57,7 +57,7 @@ contract StreamFactory is AccessControl, Ownable {
     ) public isPermittedFactoryManager returns (address streamAddress) {
         User storage user = users[_toAddress];
         require(user.hasStream == false, "User already has a stream!");
-        users[_toAddress].hasStream = true;
+        user.hasStream = true;
         // deploy a new stream contract
         SimpleStream newStream = new SimpleStream(
             _toAddress,
