63e80b064676e6b315ea72d2ccb32b08566f7af5
Sean Paterson <seanpaterson_25@msn.com>
2022-01-18 18:19:43 +0000

    Add set of streams to filter out
    Set can be easily updated to add new streams that we want to exclude.
    
    Should be pretty efficient for when there's many streams but if the stream list / black list
    are not likely to increase in size it could be overkill

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -16,7 +16,7 @@ import { SafeAppWeb3Modal } from "@gnosis.pm/safe-apps-web3modal";
 import "./App.css";
 import { Account, Contract, Faucet, GasGauge, Header, Ramp, ThemeSwitch } from "./components";
 import { INFURA_ID, NETWORK, NETWORKS } from "./constants";
-import { Transactor } from "./helpers";
+import { Transactor, filterStreamsThatAreHidden } from "./helpers";
 import { useContractConfig, useUserSigner } from "./hooks";
 import { Home, UserStream } from "./views";
 
@@ -206,9 +206,11 @@ function App(props) {
   // If you want to make 🔐 write transactions to your contracts, use the userSigner:
   const writeContracts = useContractLoader(userSigner, contractConfig);
 
-  const streams = useEventListener(readContracts, "StreamFactory", "StreamAdded", localProvider).map(s => {
-    return s.decode(s.data);
-  });
+  const rawStreams = useEventListener(readContracts, "StreamFactory", "StreamAdded", localProvider)
+  const streams = React.useMemo(() => rawStreams
+      .map(s => s.decode(s.data))
+      .filter(filterStreamsThatAreHidden),
+    [rawStreams]);
 
   // EXTERNAL CONTRACT EXAMPLE:
   //
