7cd52c3b303a557a8061d9cb6dc0cbfa3f26efb5
Jaxcoder <codenamejason@outlook.com>
2022-03-10 09:19:13 -0500

    Revert "Rinkeby deploy from Supriya"

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -2,7 +2,6 @@ const { utils } = require("ethers");
 const fs = require("fs");
 const chalk = require("chalk");
 
-require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("@tenderly/hardhat-tenderly");
 require("hardhat-deploy");
@@ -35,11 +34,11 @@ module.exports = {
       url: "http://localhost:8545",
     },
     rinkeby: {
-      url: process.env.DEPLOY_ENDPOINT_RINKEBY, // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
-      accounts: [
-        process.env.DEPLOY_ACCOUNT_RINKEBY
-      ],
+      accounts: {
+        mnemonic: mnemonic(),
+      },
     },
     kovan: {
       url: "https://kovan.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
