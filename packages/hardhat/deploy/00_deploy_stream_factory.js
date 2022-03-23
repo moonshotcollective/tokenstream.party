52e97b05f452d6f26649a5e891f63937930d4d3a
Jaxcoder <codenamejason@outlook.com>
2022-03-23 11:11:31 -0400

    update deploy script

--- packages/hardhat/deploy/00_deploy_stream_factory.js
+++ packages/hardhat/deploy/00_deploy_stream_factory.js
@@ -6,22 +6,24 @@ module.exports = async ({ getNamedAccounts, getChainId, deployments }) => {
   const { deployer } = await getNamedAccounts();
   const chainId = await getChainId();
 
-  const GTC = { address: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f" };
+  // Mainnet GTC
+  let GTC = { address: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f" };
 
-  //const owner = process.env.STREAM_FACTORY_OWNER;
-  // // deploy dummy GTC on non-mainnet networks
-  // if (chainId !== "1") {
-  //   GTC = await deploy("GTC", {
-  //     from: deployer,
-  //     args: [owner],
-  //     log: true,
-  //   });
-  // }
+  const owner = process.env.DEVELOPER;
+  // deploy dummy GTC on non-mainnet networks
+  if (chainId !== "1") {
+    GTC = await deploy("GTC", {
+      from: deployer,
+      args: [owner],
+      log: true,
+    });
+  }
 
-  // deploy the Org Factory 
+  // deploy the Org Factory
   const orgFactory = await deploy("OrgFactoryDeployer", {
     from: deployer,
     log: true,
+    args: [owner],
   });
 
   // log the GTC and orgFactoryDeployer addresses
@@ -33,6 +35,7 @@ module.exports = async ({ getNamedAccounts, getChainId, deployments }) => {
   if (chainId !== "31337") {
     await run("verify:verify", {
       address: orgFactory.address,
+      constructorArguments: [owner],
       contract: "contracts/OrgFactoryDeployer.sol:OrgFactoryDeployer",
     });
   }
